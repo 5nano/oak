913f608c98ffb4df4680236624a37fab5e89bd98
mdasilvaevora <mdasilvaevora@gmail.com>
2019-09-11 12:29:24 -0300

    Generic form v1.0

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import CropsForm from '../Crops/CropsForm';
 
 export interface AppProps { title: string }
 
+
+function Index(){
+    return <h1>Home</h1>
+}
+
 const App = (props: AppProps) => (
-    <div className="app"> Hola soy App, {props.title} </div>
+    
+    <Router>
+
+        <Switch>
+            <Route path="/" component={CropsForm}/>}
+            
+        </Switch>
+
+    </Router>
 )
+
+//return <div className="app"> Hola soy App, {props.title} </div>
+
+
+
 
 export default App;
---
+++ src/components/Crops/CropsForm.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Form } from "../Form/Form";
+import { Field } from "../Form/Field";
+
+const CropsForm: React.SFC = () => {
+  return (
+    <Form
+      action="" //endpoint
+      render={() => (
+        <React.Fragment>
+          <div className="alert alert-info" role="alert">
+            Enter the information below and we'll get back to you as soon as we
+            can.
+          </div>
+          <Field id="name" label="Name" />
+          <Field id="email" label="Email" />
+          <Field
+            id="reason"
+            label="Reason"
+            editor="dropdown"
+            options={["", "Marketing", "Support", "Feedback", "Jobs"]}
+          />
+          <Field id="notes" label="Notes" editor="multilinetextbox" />
+        </React.Fragment>
+      )}
+    />
+  );
+};
+
+export default CropsForm;
