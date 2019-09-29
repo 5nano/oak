8a7361c670ef88ba624c3a1ffab172b7b32b20db
Juan I. Biltes <juan.biltes@gmail.com>
2019-09-29 17:53:59 -0300

    herolu fix

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
  *
  */
 const devMode = process.env.NODE_ENV === 'development';
-console.log('FUCK, 'process.env.NODE_ENV);
+console.log('FUCK', process.env.NODE_ENV);
 module.exports = {
 	mode: process.env.NODE_ENV,
 
