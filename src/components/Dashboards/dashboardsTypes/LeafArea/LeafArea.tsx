a660d0e6e11edf21e4cf8f99ead9559ea8e7c847
Juan I. Biltes <juan.biltes@gmail.com>
2019-10-07 09:00:11 -0300

    Deprecated canvasJS in favor of plotly for leaf area graph

--- src/components/Dashboards/dashboardsTypes/LeafArea/LeafArea.tsx
+++ src/components/Dashboards/dashboardsTypes/LeafArea/LeafArea.tsx
@@ -4,37 +4,101 @@ import * as leafAreaScript from './LeafAreaScript';
 import { RouteComponentProps } from 'react-router-dom';
 import { withRouter } from "react-router"
 import { DashboardType } from '../InterfaceDashboardTypes';
+import Plot from 'react-plotlyjs-ts';
+import { Layout } from "plotly.js";
 
 type AssayParamsType = {
     assayId: string,
-  }
+}
+type Experiment = {
+    y: Array<number>,
+    text: Array<string>
+}
   
-  export interface LeafAreaProps extends RouteComponentProps<AssayParamsType> {
+interface LeafAreaProps extends RouteComponentProps<AssayParamsType> {
     onEmptyRender: Function
-  }
-  
+}
+interface LeafAreaState {
+    experimentsData: Array<Experiment>,
+}
+
 
 const name = "Área foliar";
-class LeafArea extends React.Component<LeafAreaProps> {
-    componentDidMount() {
-        const s = document.createElement('script');
-        s.setAttribute("id", 'nacho-script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.innerHTML = `(${leafAreaScript(this.props.match.params.assayId)})()`;
-        document.body.appendChild(s);
-    }
-    
-    componentWillUnmount() {
-        document.getElementById('nacho-script').remove();
+class LeafArea extends React.Component<LeafAreaProps, LeafAreaState> {
+    constructor(props: LeafAreaProps) {
+        super(props);
+        this.state = {
+            experimentsData: [],
+        }
+        this.fetchGraph(this.props.match.params.assayId);
     }
 
-    render() { return (
-        <>
-            <div id="chartContainer" style={{height: 370, width: '100%'}} />
-            <div id='someDiv' />
-        </>
-    )}
+    fetchGraph(assayId: string) {
+        fetch(`https://nanivo-bush.herokuapp.com/graficoComparativo/experimentos?assayId=${assayId}`,{
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+            }
+          })
+            .then(res => res.json())
+            .then(this.generateExperimentData)
+            .then((experimentsData) => {
+                this.setState({
+                    experimentsData,
+                });
+            })
+    }
+
+    generateExperimentData(data: Array<{dataPoints: Array<{label: string, y: number}>}>) {
+        const experimentsData = [];
+        for (var i = 0; i < data.length; i++) {
+            const experimentData : Experiment = { y: [], text: []};
+            for (var j = 0; j < data[i].dataPoints.length; j++) {
+                experimentData.y.push(data[i].dataPoints[j].y);
+                experimentData.text.push(data[i].dataPoints[j].label);
+            }
+            experimentsData.push(experimentData);
+        }
+        return experimentsData;
+    }
+
+    
+
+    render() { 
+        const data: Plotly.Data[] = this.state.experimentsData.map(experiment => ({
+            y: experiment.y,
+            text: experiment.text,
+            name: 'experimentos',
+            type: 'scatter'
+          }))
+    
+          const layout: Partial<Layout> = {
+            annotations: [
+              {
+                text: 'Experiment aIds',
+                x: 0,
+                xref: 'paper',
+              }
+            ],
+            title: name,
+            xaxis: {
+              title: 'Leaf Area',
+            },
+            autosize: false,
+          };
+          
+        return (
+            <>
+                <Plot
+                    data={data}
+                    layout={layout}
+                />
+                <div id='someDiv' />
+            </>
+        )
+    }
 };
 
 const leafAreaType : DashboardType = {
