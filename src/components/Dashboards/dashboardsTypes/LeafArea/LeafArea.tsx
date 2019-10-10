9b27e77f94c8b3736514c9019e1250850359a02e
Juan I. Biltes <juan.biltes@gmail.com>
2019-10-10 08:01:34 -0300

    Updated leaf area graph

--- src/components/Dashboards/dashboardsTypes/LeafArea/LeafArea.tsx
+++ src/components/Dashboards/dashboardsTypes/LeafArea/LeafArea.tsx
@@ -10,7 +10,8 @@ type AssayParamsType = {
     assayId: string,
 }
 type Experiment = {
-    y: Array<number>,
+    dates: Array<string>,
+    values: Array<number>,
     text: Array<string>,
     name: string,
 }
@@ -54,9 +55,10 @@ class LeafArea extends React.Component<LeafAreaProps, LeafAreaState> {
     generateExperimentData(data: Array<{dataPoints: Array<{label: string, y: number}>}>) {
         const experimentsData = [];
         for (var i = 0; i < data.length; i++) {
-            const experimentData : Experiment = { y: [], text: [], name: `Experimento ${i}`};
+            const experimentData : Experiment = { dates:[], values: [], text: [], name: `Experimento ${i}`};
             for (var j = 0; j < data[i].dataPoints.length; j++) {
-                experimentData.y.push(data[i].dataPoints[j].y);
+                experimentData.dates.push(`2019-08-${Math.floor(Math.random()*30 + 1)}`);
+                experimentData.values.push(data[i].dataPoints[j].y);
                 experimentData.text.push(data[i].dataPoints[j].label);
             }
             experimentsData.push(experimentData);
@@ -68,27 +70,44 @@ class LeafArea extends React.Component<LeafAreaProps, LeafAreaState> {
 
     render() { 
         const data: Plotly.Data[] = this.state.experimentsData.map(experiment => ({
-            y: experiment.y,
-            text: experiment.text,
+            x: experiment.dates,
+            y: experiment.values,
             name: experiment.name,
             type: 'scatter'
           }))
 
           const layout: Partial<Layout> = {
-            annotations: [
-              {
-                text: 'Experiment aIds',
-                x: 0,
-                xref: 'paper',
-              }
-            ],
+              showlegend: true,
+
             title: name,
             xaxis: {
-              title: 'Leaf Area',
+              type: 'date',
+              autorange: true,
+              rangeselector: {buttons: [
+                {
+                  count: 7,
+                  label: '1w',
+                  step: 'day',
+                  stepmode: 'todate'
+                },
+                {
+                  count: 1,
+                  label: '1m',
+                  step: 'month',
+                  stepmode: 'backward'
+                },
+                {step: 'all'}
+              ]},
+            rangeslider: {},
+            },
+            yaxis: {
+                tickformat: '.3s', // Hasta 3 dígitos
+                ticksuffix: " mm2",
+                showticksuffix: "all"
             },
             autosize: false,
           };
-          
+
         return (
             <>
                 <Plot
