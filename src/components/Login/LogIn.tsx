eea88cd97b767d22baa62cc6393aee2201ca1414
Juan I. Biltes <juan.biltes@gmail.com>
2019-10-13 18:43:11 -0300

    Fix cookie, laughing

--- src/components/Login/LogIn.tsx
+++ src/components/Login/LogIn.tsx
@@ -28,7 +28,7 @@ export class LogIn extends React.Component<ILogInProps,ILogInState> {
 
        BushService.put('/usuarios/validar', this.state)
         .then(response => {
-            document.cookie = `user=${this.state.username};max-age=60*60*24*365;`
+            document.cookie = `user=${this.state.username};max-age=${60*60*24*365};`
             this.props.validateLogin();
         });
       }
