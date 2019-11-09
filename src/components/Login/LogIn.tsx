7c4193da94ae9c50066689631d92a06524109156
mdasilvaevora <mdasilvaevora@gmail.com>
2019-11-09 12:51:19 -0300

    redesign login

--- src/components/Login/LogIn.tsx
+++ src/components/Login/LogIn.tsx
@@ -5,7 +5,8 @@ import { RouteComponentProps } from "react-router-dom";
 
 export interface ILogInState {
     username:string,
-    password:string
+    password:string,
+    loading:boolean
 }
 
 export interface ILogInProps extends RouteComponentProps{
@@ -20,6 +21,7 @@ export class LogIn extends React.Component<ILogInProps,ILogInState> {
         this.state = {
             username: '',
             password: '',
+            loading: false
         }
     }
 
@@ -27,49 +29,66 @@ export class LogIn extends React.Component<ILogInProps,ILogInState> {
         e: React.FormEvent<HTMLFormElement>
       ): Promise<void> => {
        e.preventDefault();
+       this.setState({loading:true})
 
-       BushService.post('/login', this.state)
+       let loginInfo = {
+           username:this.state.username,
+           password:this.state.password
+       }
+       BushService.post('/login', loginInfo)
         .then(response => {
             document.cookie = `user=${this.state.username};max-age=${60*60*24*365};`
             this.props.validateLogin();
             this.props.history.push('/home')
+            this.setState({loading:false})
         });
       }
 
-    private handleChange =  (
+    private handleChangeUsername =  (
         event: React.ChangeEvent<HTMLInputElement>
     ): void => {
-        const newState = {[event.target.id]: event.target.value} as Pick <ILogInState,keyof ILogInState>;
-        this.setState(newState);
+        this.setState({username:event.target.value})
     }
+
+    private handleChangePassword =  (
+        event: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        this.setState({password:event.target.value})
+    }
+
 
     render(){
     return(
         <div className="login-container">
             <form className="login-form" onSubmit={this.handleSubmit}>
-                <input
-                    id='username'
-                    className="username input"
-                    type="text"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                    placeholder="Username"
-                    />
-                    <p className="help" onClick={()=>this.props.history.push('/register')}>No tenes una cuenta?  
-                        <a>Registrate ahora</a>
+                <div className="login-input">
+                    <input
+                        id='username'
+                        className="username input"
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleChangeUsername}
+                        placeholder="Username"
+                        />
+                        <p className="help" onClick={()=>this.props.history.push('/register')}>No tenes una cuenta?  
+                            <a>Registrate ahora</a>
+                        </p>
+                </div>
+
+                <div className="login-input">
+                    <input
+                        id='password'
+                        className="password input"
+                        type="password"
+                        value={this.state.password}
+                        onChange={this.handleChangePassword}
+                        placeholder="Password"
+                        />
+                    <p className="help">  
+                        <a >Olvidate tu contraseña?</a>
                     </p>
-                <input
-                    id='password'
-                    className="password input"
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    placeholder="Password"
-                    />
-                <p className="help">  
-                    <a >Olvidate tu contraseña?</a>
-                </p>
-                <Button type="submit" title="Ingresar"/>
+                </div>
+                <Button type="submit" title={this.state.loading? "Verificando...":"Ingresar"}/>
             </form>
         </div>
     )
